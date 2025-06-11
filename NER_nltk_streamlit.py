4cf0af691fbaa29de71485573634733c60b0f9a9
Jennie Williams <35732437+jenniepython@users.noreply.github.com>
2025-06-11 18:32:58 +0100

    Update NER_nltk_streamlit.py

--- NER_nltk_streamlit.py
+++ NER_nltk_streamlit.py
@@ -27,7 +27,7 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 
-name, auth_status, username = authenticator.login("Login", location="main")
+name, auth_status, username = authenticator.login("Login", "main")
 
 if auth_status:
     authenticator.logout("Logout", "sidebar")
